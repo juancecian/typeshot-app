82a3dda2a13c528565cd7f03f5c2309c0044119f
Juan <franco.ciancio23@gmail.com>
2023-06-23 03:59:58 -0300

    Configurado scroll al abrir el teclado en ambos dispositivos

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -4,50 +4,96 @@ import {
   Heading,
   Input,
   Link,
+  ScrollView,
   Stack,
   VStack,
-  View
+  View,
+  KeyboardAvoidingView
 } from 'native-base';
+import { Platform } from 'react-native';
 import Animated, { FadeIn } from 'react-native-reanimated';
 
 const LoginScreen = () => {
   return (
-    <Animated.View entering={FadeIn} style={{ flex: 1 }}>
-      <VStack
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-      >
-        <Heading>SocialMedia</Heading>
-        <View mt={10}>
-          <Heading fontSize={17}>Inicia sesión con tus datos</Heading>
-          <Heading fontSize={17} underline textAlign="center">
-            o crea una nueva cuenta
-          </Heading>
-        </View>
-        <Stack w="90%" p={10} space={5}>
-          <FormControl>
-            <Input
-              type="text"
-              size="lg"
-              variant="filled"
-              placeholder="Nombre de usuario"
-            />
-          </FormControl>
-          <FormControl>
-            <Input
-              type="password"
-              size="lg"
-              variant="filled"
-              placeholder="Contraseña"
-            />
-          </FormControl>
-          <Button w="50%" mx="auto" size="sm" variant="subtle">
-            INICIAR SESIÓN
-          </Button>
-          <Link>Olvidé mi contraseña</Link>
-          <Link>Olvidé mi usuario</Link>
-        </Stack>
-      </VStack>
-    </Animated.View>
+    <KeyboardAvoidingView
+      flex={1}
+      mt="10%"
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+    >
+      <Animated.View entering={FadeIn} style={{ flex: 1 }}>
+        <ScrollView
+          flex={1}
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+        >
+          <VStack justifyContent="center" alignItems="center">
+            <Heading>SocialMedia</Heading>
+            <View mt={10}>
+              <Heading fontSize={17}>Inicia sesión con tus datos</Heading>
+              <Heading fontSize={17} underline textAlign="center">
+                o crea una nueva cuenta
+              </Heading>
+            </View>
+            <Stack w="90%" p={10} space={5}>
+              <FormControl>
+                <Input
+                  type="text"
+                  size="lg"
+                  variant="filled"
+                  placeholder="Nombre de usuario"
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  type="password"
+                  size="lg"
+                  variant="filled"
+                  placeholder="Contraseña"
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  type="password"
+                  size="lg"
+                  variant="filled"
+                  placeholder="Contraseña"
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  type="password"
+                  size="lg"
+                  variant="filled"
+                  placeholder="Contraseña"
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  type="password"
+                  size="lg"
+                  variant="filled"
+                  placeholder="Contraseña"
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  type="password"
+                  size="lg"
+                  variant="filled"
+                  placeholder="Contraseña"
+                />
+              </FormControl>
+              <Button w="50%" mx="auto" size="sm" variant="subtle">
+                INICIAR SESIÓN
+              </Button>
+              <Link>Olvidé mi contraseña</Link>
+              <Link>Olvidé mi usuario</Link>
+            </Stack>
+          </VStack>
+        </ScrollView>
+      </Animated.View>
+    </KeyboardAvoidingView>
   );
 };
 
