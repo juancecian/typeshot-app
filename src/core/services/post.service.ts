35c0709a1f553562516f0800c01f7eaea5171c32
Juan <franco.ciancio23@gmail.com>
2023-06-24 04:37:30 -0300

    Creado servicio para publicaciones

---
+++ src/core/services/post.service.ts
@@ -0,0 +1,34 @@
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import app from '../../config/firebase.config';
+import { PostModel } from '../../models/post.model';
+
+const db = getFirestore(app);
+
+export const getPosts = (userIds: string[]): Promise<PostModel[]> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const posts: PostModel[] = [];
+      for (const userId of userIds) {
+        const postCol = collection(db, `Users/${userId}/post_information`);
+        const postSnapshot = await getDocs(postCol);
+        const userCol = collection(db, `Users/${userId}/user_information`);
+        const userSnapshot = await getDocs(userCol);
+        postSnapshot.docs.map((doc) => {
+          let docData = new PostModel();
+          docData.id = doc.data().id;
+          docData.text = doc.data().text;
+          docData.images = doc.data().images;
+          docData.author.id = userSnapshot.docs[0].data().id;
+          docData.author.name = userSnapshot.docs[0].data().fullName;
+          docData.author.username = userSnapshot.docs[0].data().username;
+          docData.author.avatar = userSnapshot.docs[0].data().avatar;
+          posts.push(docData);
+        });
+      }
+      console.log(posts);
+      resolve(posts);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
