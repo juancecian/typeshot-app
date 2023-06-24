e6d105404f3f9e78808ee35cf00ba312dff7de71
Juan <franco.ciancio23@gmail.com>
2023-06-24 04:34:39 -0300

    Configurado context para guardar info del usuario loguead

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { theme } from 'native-base';
 import { createContext, useEffect, useState } from 'react';
+import { UserModel } from '../models/user.model';
 
 const ThemeContext = createContext<{
   colorMode: string;
@@ -10,8 +11,17 @@ const ThemeContext = createContext<{
   onColorModeChange: () => {}
 });
 
+const UserContext = createContext<{
+  user: UserModel | null;
+  updateUser: (user: UserModel) => void;
+}>({
+  user: null,
+  updateUser: () => {}
+});
+
 export const AppProvider = ({ children }: { children: any }) => {
   const [colorMode, setColorMode] = useState('');
+  const [user, setUser] = useState<UserModel>();
 
   useEffect(() => {
     loadThemeMode();
@@ -37,16 +47,33 @@ export const AppProvider = ({ children }: { children: any }) => {
     }
   };
 
+  const handleUpdateUser = async (user: UserModel) => {
+    try {
+      if (user) {
+        setUser(user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const themeContextValue = {
     colorMode: colorMode,
     onColorModeChange: handleColorModeChange
   };
 
+  const userContextValue = {
+    user: user || null,
+    updateUser: handleUpdateUser
+  };
+
   return (
     <ThemeContext.Provider value={themeContextValue}>
-      {children}
+      <UserContext.Provider value={userContextValue}>
+        {children}
+      </UserContext.Provider>
     </ThemeContext.Provider>
   );
 };
 
-export { ThemeContext };
+export { ThemeContext, UserContext };
