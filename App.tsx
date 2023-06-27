82b096f705c08e6fa5bce732efd5229991dc159e
Juan <franco.ciancio23@gmail.com>
2023-06-27 03:27:27 -0300

    Eliminado ScreenLoader inutilizable y actualizados import en desuso

--- App.tsx
+++ App.tsx
@@ -10,7 +10,6 @@ import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import HomeScreen from './src/screens/Home/HomeScreen';
 import Tabs from './src/screens/Tabs/Tabs';
-import ScreenLoader from './src/components/ScreenLoader';
 
 const Stack = createNativeStackNavigator();
 
