114530d49a5dd902fee8b27233ded435c91be6d3
Juan <franco.ciancio23@gmail.com>
2023-06-27 03:30:09 -0300

    Cambiados algunos estilos del theme

--- src/screens/User/ProfileScreen.tsx
+++ src/screens/User/ProfileScreen.tsx
@@ -6,6 +6,7 @@ import { SceneMap, TabBar, TabView } from 'react-native-tab-view';
 import PostsPage from './ProfilePages/PostsPage';
 import PhotosPage from './ProfilePages/PhotosPage';
 import { useWindowDimensions } from 'react-native';
+import BottomSheetProfile from '../../components/BottomSheetProfile';
 
 const renderScene = SceneMap({
   first: PostsPage,
@@ -25,33 +26,37 @@ const ProfileScreen = () => {
   ]);
 
   return (
-    <Animated.View style={{ flex: 1 }}>
-      <VStack
-        flex={1}
-        bg={colorMode === 'light' ? 'warmGray.50' : 'coolGray.800'}
-      >
-        <TabView
-          navigationState={{ index, routes }}
-          renderScene={renderScene}
-          onIndexChange={setIndex}
-          style={{ marginTop: '10%' }}
-          initialLayout={{ width: layout.width }}
-          renderTabBar={(props) => (
-            <TabBar
-              {...props}
-              indicatorStyle={{
-                backgroundColor: colorMode === 'light' ? 'black' : 'white'
-              }}
-              style={{
-                backgroundColor:
-                  colorMode === 'light' ? 'warmGray.50' : 'coolGray.800'
-              }}
-              labelStyle={{ color: colorMode === 'light' ? 'black' : 'white' }}
-            />
-          )}
-        />
-      </VStack>
-    </Animated.View>
+    <>
+      <Animated.View style={{ flex: 1 }}>
+        <VStack
+          flex={1}
+          bg={colorMode === 'light' ? 'warmGray.50' : 'coolGray.800'}
+        >
+          <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            style={{ marginTop: '10%' }}
+            initialLayout={{ width: layout.width }}
+            renderTabBar={(props) => (
+              <TabBar
+                {...props}
+                indicatorStyle={{
+                  backgroundColor: colorMode === 'light' ? 'black' : 'white'
+                }}
+                style={{
+                  backgroundColor:
+                    colorMode === 'light' ? 'warmGray.50' : 'coolGray.800'
+                }}
+                labelStyle={{
+                  color: colorMode === 'light' ? 'black' : 'white'
+                }}
+              />
+            )}
+          />
+        </VStack>
+      </Animated.View>
+    </>
   );
 };
 
